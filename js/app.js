b7701118ca3f153928eade63606abd68063d096a
Jym Bocala <jym.bocala@gmail.com>
2022-07-20 00:30:32 +1000

    add navbar in js based on html sections

--- js/app.js
+++ js/app.js
@@ -39,7 +39,41 @@
 */
 
 // build the nav
+/**
+ *
+ * 
+ * 
+ *  
+ */
 
+//selects all sections in the html
+const allSections = document.querySelectorAll('.section_group');
+//select ul in navbar
+const navList = document.querySelector('#navbar__list');
+
+//loop through each section
+for (const section of allSections) {
+    console.log(section.getAttribute('data-nav'));
+    // create new <a> tag
+    var newA = document.createElement('a');
+    // a variable that set text of each <li> item as the data-nav in each section
+    var newAText = section.getAttribute('data-nav');
+
+    //append text node to new <a> tag
+    var textNode = document.createTextNode(newAText);
+    newA.appendChild(textNode);
+
+    //create new <li>, with a class to be used for styling
+    var newLi = document.createElement('li');
+    newLi.classList.add('navbar_item');
+
+    // set new <a> tag's href with the section id
+    newA.href = '#' + section.id;
+
+    // append new <a> within new <li>, then add it to navList
+    newLi.appendChild(newA);
+    navList.appendChild(newLi);    
+}
 
 // Add class 'active' to section when near top of viewport
 
