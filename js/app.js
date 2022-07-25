1245b71b80f4c49f82c8649b62fec8b13293a962
Jym Bocala <jym.bocala@gmail.com>
2022-07-25 13:30:33 +1000

    add active class function + scrollIntoView function

--- js/app.js
+++ js/app.js
@@ -53,30 +53,63 @@ const navList = document.querySelector('#navbar__list');
 
 //loop through each section
 for (const section of allSections) {
-    console.log(section.getAttribute('data-nav'));
-    // create new <a> tag
-    var newA = document.createElement('a');
-    // a variable that set text of each <li> item as the data-nav in each section
-    var newAText = section.getAttribute('data-nav');
-
-    //append text node to new <a> tag
-    var textNode = document.createTextNode(newAText);
+    // console.log(section.getAttribute('data-nav'));
+    const sectionId = section.id;
+    const newA = document.createElement('a'); // create new <a> tag
+    const newAText = section.getAttribute('data-nav'); // a variable that set text of each <li> item as the data-nav in each section
+    const textNode = document.createTextNode(newAText); //append text node to new <a> tag
     newA.appendChild(textNode);
+    const newLi = document.createElement('li'); //create new <li>, with a class to be used for styling
 
-    //create new <li>, with a class to be used for styling
-    var newLi = document.createElement('li');
-    newLi.classList.add('navbar_item');
+    newLi.classList.add('menu__link');
+    newA.href = '#' + sectionId; // set new <a> tag's href with the section id
+    newLi.appendChild(newA); // append new <a> within new <li>, then add it to navList
+    navList.appendChild(newLi);
 
-    // set new <a> tag's href with the section id
-    newA.href = '#' + section.id;
+// scroll into view sections
+    // console.log(newA);
+    newA.addEventListener('click', function (event) {
+        event.preventDefault();
+        section.scrollIntoView ({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
 
-    // append new <a> within new <li>, then add it to navList
-    newLi.appendChild(newA);
-    navList.appendChild(newLi);    
-}
+// SCROLL
+// 1. add event listeners to each link in navbar
+//      click event
+// 2. function callback
+//      call prevent default behaviour for clicking links (before)
+//      get the target id (sections in the html)
+// 3.   use scrollintoview w/ target id    
+
+
 
 // Add class 'active' to section when near top of viewport
-
+window.addEventListener('scroll', highlight); // highlight section in viewport to yellow
+// function to add/remove #your-active-class in each section
+function highlight() { 
+     for (i = 0; i < allSections.length; i++) {
+         if(!isVisible(allSections[i])){
+             allSections[i].classList.remove('your-active-class');
+         } else {
+             allSections[i].classList.add('your-active-class');
+         }
+     }
+ }
+// function to define the sections' position in viewport
+function isVisible(element) { 
+     let bounding = element.getBoundingClientRect();
+     return (
+         bounding.top + 300 >= 0 &&
+         bounding.left >= 0 &&
+         bounding.bottom <= (window.innerHeight) + 200 &&
+         bounding.right <= (window.innerWidth)
+     );
+ };
+ 
 
 // Scroll to anchor ID using scrollTO event
 
@@ -88,8 +121,10 @@ for (const section of allSections) {
 */
 
 // Build menu 
-
-// Scroll to section on link click
-
+ 
 // Set sections as active
+
+
+
+
 
